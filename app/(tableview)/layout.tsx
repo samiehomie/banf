4173f0202bccb3a5b5a86445f2d275f276eea2bc
Jeong Sam <samie.worker@gmail.com>
2023-12-18 15:27:37 +0900

    Add text

--- app/(tableview)/layout.tsx
+++ app/(tableview)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className="flex mt-[25px] w-[1550px] h-[954px] 
+      className="flex mt-[25px] w-[1550px] h-[954px]  
     bg-white rounded-[12px] shadow-custom border border-[#ECEEF6] px-[30px] relative"
     >
       {children}
